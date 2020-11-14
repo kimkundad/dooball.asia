e07d537269cca4fe6d5ce8e3d5257821157668ac
kimkundad <kim.kundad@gmail.com>
2020-11-14 21:17:17 +0700

    kim

--- app/Http/Controllers/API/DooballScraperController.php
+++ app/Http/Controllers/API/DooballScraperController.php
@@ -155,6 +155,7 @@ class DooballScraperController extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $data = curl_exec($ch);
