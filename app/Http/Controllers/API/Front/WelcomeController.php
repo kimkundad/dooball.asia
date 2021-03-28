a9fced5e1728537a899e799ee31cebd32a7e421f
kimkundad <kim.kundad@gmail.com>
2021-03-28 11:07:22 +0700

    no message

--- app/Http/Controllers/API/Front/WelcomeController.php
+++ app/Http/Controllers/API/Front/WelcomeController.php
@@ -165,7 +165,7 @@ class WelcomeController extends Controller
             // DB::enableQueryLog();
             $matches = Match::whereBetween('match_time', [$mid_this_date, $ten_tomorrow_date]);
             $get_all_team = Match::whereBetween('match_time', [$mid_this_date, $ten_tomorrow_date]);
-
+            $get_all_team->where('match_name', $page->league_name);
 
 
           //  dd($pageCondition);
@@ -173,7 +173,7 @@ class WelcomeController extends Controller
             if ($pageCondition == 'L') {
                 // $matches->where('match_name', 'LIKE', '%' . $league_name . '%');
                 $matches->where('match_name', $page->league_name);
-                $get_all_team->where('match_name', $page->league_name);
+
 
             } else if ($pageCondition == 'T') {
 
